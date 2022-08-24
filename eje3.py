fb3dd6dda02775433e505d09538723080a2cff28
tortuguitahack <104583107+tortuguitahack@users.noreply.github.com>
2022-08-24 12:09:56 -0400

    Add files via upload

---
+++ eje3.py
@@ -0,0 +1,12 @@
+print("diego")
+print("cortez")
+print("36")
+print("diegoedgardocortezyanez@gmail.com")
+print("00000000")
+print("falso")
+print("verdadero")
+print("pedro, pablo")
+diccionario = {
+    "Accion":"Guns","Comedia":"why him"}
+print(diccionario.get("Accion"))
+print(diccionario.get("Comedia"))
