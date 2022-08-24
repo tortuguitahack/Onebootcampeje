a1152b66873c7dd3c26331e8226c50b6066c5ab9
tortuguitahack <104583107+tortuguitahack@users.noreply.github.com>
2022-08-24 15:37:27 -0400

    Add files via upload

---
+++ eje4,1.py
@@ -0,0 +1,18 @@
+#Numeros impares
+
+def impares(numeros) :
+  numeros_impares = ()
+
+  for n in numeros_impares:
+    if n % 2 != 0:
+        numeros_impares.append(n)
+    return numeros_impares
+    numeros = (2, 3, 4, 5, 6, 7, 8)
+
+    resultado = impares(numeros)
+
+    print(numeros_impares)
+    print(resultado)
+
+  
+
