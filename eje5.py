24cf7cf9ec8d1abb364d3c30c2836e362ec2c2db
tortuguitahack <104583107+tortuguitahack@users.noreply.github.com>
2022-08-24 19:15:54 -0400

    Add files via upload

---
+++ eje5.py
@@ -0,0 +1,9 @@
+import math
+def area_triangulo(base, altura):
+    return base * altura /2
+print(area_triangulo(3, 6))
+
+def area_circulo(radio):
+    return math.pi * radio **2
+
+print(area_circulo(5))    
